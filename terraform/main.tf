3b903263ed338c59064b7b73ec4bd384eb429b9d
shrey <shreyr7@gmail.com>
2025-02-13 03:50:23 -0600

    Updated main.tf to include created key-pair shr-terra

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ module "web_server" {
   source        = "./modules/ec2_web_server"
   ami_id        = "ami-03d49b144f3ee2dc4" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
-  key_name      = "my-key-pair"
+  key_name      = "shr-terra"
 }
 
 output "web_server_ip" {
