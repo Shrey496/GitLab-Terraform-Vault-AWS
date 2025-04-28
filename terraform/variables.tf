abdea628fd39a05aa3b273c2251a1211fa51dda1
Shrey Raut <shreyr7@gmail.com>
2025-04-28 04:34:36 +0000

    Update 2 files
    - /.gitlab-ci.yml
    - /terraform/variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,18 @@ variable "aws_region" {
   type        = string
   default     = "us-west-1"
 }
+
+variable "address" {
+  description = "Vault address"
+  type        = string
+}
+
+variable "role_id" {
+  description = "Vault AppRole role_id"
+  type        = string
+}
+
+variable "secret_id" {
+  description = "Vault AppRole secret_id"
+  type        = string
+}
